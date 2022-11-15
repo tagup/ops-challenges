31888c15d3e77da73ae82ea27d19146e3be0ea9e
saumitrank <saumitrank@gmail.com>
2022-11-14 19:28:53 -0600

    preliminary analysis for one machine

--- data-science/utils.py
+++ data-science/utils.py
@@ -1,17 +1,12 @@
 """Module that contains utility functions  """
-import pandas as pd
 import numpy as np
+import pandas as pd
+from scipy import signal
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
 
 def denoise_data(data:pd.DataFrame, window=10)->pd.DataFrame:
-    """_summary_
-
-    Args:
-        data (pd.DataFrame): Input timeseries for a single machine
-        window (int): Moving average window
-
-    Returns:
-        pd.DataFrame: Data with noise removed
-    """
 
     data = data.copy()
 
@@ -32,4 +27,36 @@ def denoise_data(data:pd.DataFrame, window=10)->pd.DataFrame:
         data[sensor] = y.interpolate().rolling(window=window, min_periods=1).mean()
 
     return data
+
+
+def spectrogram_data(data:pd.DataFrame, nperseg=64)->pd.DataFrame:
     
+    spectra = []
+
+    for sensor in data.columns:
+        
+        f, t, Sxx = signal.spectrogram(data[sensor], nperseg=nperseg)
+
+        index = pd.Series(t.astype(int), name="time")
+        columns = pd.MultiIndex.from_tuples([(sensor, x) for x in f], names=["sensor", "frequency"])
+
+        spectra.append(pd.DataFrame(Sxx.T, index=index, columns=columns))
+
+    return pd.concat(spectra, axis=1)
+
+def pca_spectrogram(spec_data:pd.DataFrame, n_components=4)->pd.DataFrame:
+
+    pca_data = []
+
+    index = spec_data.index
+
+    for sensor in spec_data.columns.unique(0):
+
+        scaler = StandardScaler()
+        X = scaler.fit_transform(spec_data[sensor].values)
+
+        pca = PCA(n_components=n_components)
+        columns = pd.MultiIndex.from_tuples([(sensor, i) for i in range(n_components)], names=["sensor", "freq component"])
+        pca_data.append(pd.DataFrame(pca.fit_transform(X), index=index, columns=columns))
+    
+    return pd.concat(pca_data, axis=1)
